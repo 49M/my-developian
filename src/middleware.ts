78b17c19c7dda9c29694fc81b189771af7334880
49M <michal_buczek@yahoo.com>
2025-06-10 15:04:22 -0400

    back

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,13 @@
 import { NextResponse, type NextRequest } from 'next/server';
-import { updateSession } from '@/utils/supabase/middleware';
+// import { updateSession } from '@/utils/supabase/middleware';
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
-}
-
-// export function middleware(request: NextRequest) {
-//   return NextResponse.next();
+// export async function middleware(request: NextRequest) {
+//   return await updateSession(request);
 // }
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
