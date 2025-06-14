fb967b404a807df2cf93800497641a9ba5200298
49M <michal_buczek@yahoo.com>
2025-06-13 21:40:36 -0400

    title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,9 @@ export default function Page() {
 
   return (
     <div className='p-8'>
+      <h1 className='mt-[20px] flex justify-center font-roboto text-[45px] font-semibold'>
+        My Developian
+      </h1>
       <h1 className='text-xl font-bold'>{user ? `Welcome, ${user.email}` : 'Hello, Guest'}</h1>
       {user && (
         <button onClick={() => signOut()} className='mt-4 rounded border px-4 py-2'>
