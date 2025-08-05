44e2e188c4314467a1bb22118e48e9a964601a7f
49M <michal_buczek@yahoo.com>
2025-08-04 23:10:38 -0400

    langraph initial

---
+++ src/lib/langgraph/roadmapGraph.ts
@@ -0,0 +1,19 @@
+// import { TavilySearch } from '@langchain/tavily';
+// import { StateGraph } from '@langchain/langgraph';
+// import { cohere } from '@/utils/cohere';
+// import { RunnableLambda } from '@langchain/core/runnables';
+
+// type RoadmapState = {
+//   userInput: string;
+//   searchResults?: string;
+//   strategy?: string;
+//   outline?: string;
+//   tasks?: string;
+//   schedule?: string;
+//   finalOutput?: string;
+// };
+
+// const webSearch = RunnableLambda.from(async (state: RoadmapState) => {
+//   const search = new TavilySearch();
+//   const query = ``;
+// });
